08669f6e8ae5d54b2cd1244afaa6b3394da2dd67
ShwetanshuSingh <shwetanshu_singh@yahoo.com>
2017-09-01 16:32:41 +0530

    Updated Density Values

--- data/data.js
+++ data/data.js
@@ -4,14 +4,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 878,
       "name": "Andaman and Nicobar Islands",
-      "area": 8249,
       "density": 46,
-      "literacy": 86.3,
       "num_schools": 410,
-      "population": 380000
+      "area": 8249,
+      "enrollment_sex_ratio_I_V": 960,
+      "population": 380000,
+      "literacy": 86.3
     }
   },
   "AP": {
@@ -19,29 +19,29 @@ var stateData = {
       "std_v_division": 37.2
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 930,
       "sex_ratio": 992,
       "name": "Andhra Pradesh",
-      "area": 275045,
-      "density": 308,
-      "literacy": 67.70,
+      "density": 307,
       "num_schools": 60435,
-      "population": 84666000
+      "area": 275045,
+      "enrollment_sex_ratio_I_V": 930,
+      "population": 84666000,
+      "literacy": 67.7
     }
   },
   "AR": {
     "ASER": {
-      "std_v_division": 19.0
+      "std_v_division": 19
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 950,
       "sex_ratio": 920,
       "name": "Arunachal Pradesh",
-      "area": 83743,
-      "density": 17,
-      "literacy": 67.0,
+      "density": 16,
       "num_schools": 4012,
-      "population": 1383000
+      "area": 83743,
+      "enrollment_sex_ratio_I_V": 950,
+      "population": 1383000,
+      "literacy": 67
     }
   },
   "AS": {
@@ -49,14 +49,14 @@ var stateData = {
       "std_v_division": 13.6
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 980,
       "sex_ratio": 954,
       "name": "Assam",
-      "area": 78438,
-      "density": 398,
-      "literacy": 73.2,
+      "density": 397,
       "num_schools": 65894,
-      "population": 31169000
+      "area": 78438,
+      "enrollment_sex_ratio_I_V": 980,
+      "population": 31169000,
+      "literacy": 73.2
     }
   },
   "BR": {
@@ -64,14 +64,14 @@ var stateData = {
       "std_v_division": 32.6
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 980,
       "sex_ratio": 916,
       "name": "Bihar",
-      "area": 94163,
-      "density": 1106,
-      "population": 103805000,
+      "density": 1102,
       "num_schools": 80166,
-      "literacy": 63.8
+      "area": 94163,
+      "enrollment_sex_ratio_I_V": 980,
+      "literacy": 63.8,
+      "population": 103805000
     }
   },
   "CH": {
@@ -79,29 +79,29 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 880,
       "sex_ratio": 818,
       "name": "Chandigarh",
-      "area": 114,
-      "density": 9258,
-      "literacy": 86.4,
+      "density": 9254,
       "num_schools": 201,
-      "population": 1055000
+      "area": 114,
+      "enrollment_sex_ratio_I_V": 880,
+      "population": 1055000,
+      "literacy": 86.4
     }
   },
   "CT": {
     "ASER": {
-      "std_v_division": 23.0
+      "std_v_division": 23
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 991,
       "name": "Chhattisgarh",
-      "area": 135191,
-      "density": 189,
-      "literacy": 71.0,
+      "density": 188,
       "num_schools": 50705,
-      "population": 25540000
+      "area": 135191,
+      "enrollment_sex_ratio_I_V": 960,
+      "population": 25540000,
+      "literacy": 71
     }
   },
   "DD": {
@@ -109,14 +109,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 860,
       "sex_ratio": 618,
       "name": "Daman and Diu",
-      "area": 112,
-      "density": 2191,
-      "literacy": 87.1,
+      "density": 2169,
       "num_schools": 120,
-      "population": 243000
+      "area": 112,
+      "enrollment_sex_ratio_I_V": 860,
+      "population": 243000,
+      "literacy": 87.1
     }
   },
   "DL": {
@@ -124,14 +124,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 870,
       "sex_ratio": 866,
       "name": "Delhi",
-      "area": 1483,
-      "density": 11320,
-      "population": 16753000,
+      "density": 11296,
       "num_schools": 5751,
-      "literacy": 86.3
+      "area": 1483,
+      "enrollment_sex_ratio_I_V": 870,
+      "literacy": 86.3,
+      "population": 16753000
     }
   },
   "DN": {
@@ -139,14 +139,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 880,
       "sex_ratio": 775,
       "name": "Dadra and Nagar Haveli",
-      "area": 451,
-      "density": 700,
-      "population": 343000,
+      "density": 760,
       "num_schools": 323,
-      "literacy": 77.7
+      "area": 451,
+      "enrollment_sex_ratio_I_V": 880,
+      "literacy": 77.7,
+      "population": 343000
     }
   },
   "GA": {
@@ -154,14 +154,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 920,
       "sex_ratio": 968,
       "name": "Goa",
-      "area": 3702,
-      "density": 394,
-      "literacy": 87.4,
+      "density": 393,
       "num_schools": 1462,
-      "population": 1458000
+      "area": 3702,
+      "enrollment_sex_ratio_I_V": 920,
+      "population": 1458000,
+      "literacy": 87.4
     }
   },
   "GJ": {
@@ -169,14 +169,14 @@ var stateData = {
       "std_v_division": 16.1
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 890,
       "sex_ratio": 918,
       "name": "Gujarat",
-      "area": 196024,
       "density": 308,
-      "literacy": 79.3,
       "num_schools": 44051,
-      "population": 60384000
+      "area": 196024,
+      "enrollment_sex_ratio_I_V": 890,
+      "population": 60384000,
+      "literacy": 79.3
     }
   },
   "HP": {
@@ -184,14 +184,14 @@ var stateData = {
       "std_v_division": 53.7
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 910,
       "sex_ratio": 974,
       "name": "Himachal Pradesh",
-      "area": 55673,
       "density": 123,
-      "population": 6857000,
       "num_schools": 18024,
-      "literacy": 83.8
+      "area": 55673,
+      "enrollment_sex_ratio_I_V": 910,
+      "literacy": 83.8,
+      "population": 6857000
     }
   },
   "HR": {
@@ -199,14 +199,14 @@ var stateData = {
       "std_v_division": 48.9
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 830,
       "sex_ratio": 877,
       "name": "Haryana",
-      "area": 44212,
       "density": 573,
-      "population": 25353000,
       "num_schools": 22268,
-      "literacy": 76.6
+      "area": 44212,
+      "enrollment_sex_ratio_I_V": 830,
+      "literacy": 76.6,
+      "population": 25353000
     }
   },
   "JH": {
@@ -214,14 +214,14 @@ var stateData = {
       "std_v_division": 23.5
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 947,
       "name": "Jharkhand",
-      "area": 79714,
-      "density": 414,
-      "population": 32966000,
+      "density": 413,
       "num_schools": 47441,
-      "literacy": 67.6
+      "area": 79714,
+      "enrollment_sex_ratio_I_V": 960,
+      "literacy": 67.6,
+      "population": 32966000
     }
   },
   "JK": {
@@ -229,14 +229,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 920,
       "sex_ratio": 883,
       "name": "Jammu and Kashmir",
-      "area": 222236,
       "density": 56,
-      "population": 12549000,
       "num_schools": 28578,
-      "literacy": 68.7
+      "area": 222236,
+      "enrollment_sex_ratio_I_V": 920,
+      "literacy": 68.7,
+      "population": 12549000
     }
   },
   "KA": {
@@ -244,14 +244,14 @@ var stateData = {
       "std_v_division": 19.7
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 940,
       "sex_ratio": 968,
       "name": "Karnataka",
-      "area": 191791,
-      "density": 319,
-      "literacy": 75.6,
+      "density": 318,
       "num_schools": 61739,
-      "population": 61131000
+      "area": 191791,
+      "enrollment_sex_ratio_I_V": 940,
+      "population": 61131000,
+      "literacy": 75.6
     }
   },
   "KL": {
@@ -259,14 +259,14 @@ var stateData = {
       "std_v_division": 38.6
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 1084,
       "name": "Kerala",
-      "area": 38863,
-      "density": 860,
-      "literacy": 93.9,
+      "density": 859,
       "num_schools": 16458,
-      "population": 33388000
+      "area": 38863,
+      "enrollment_sex_ratio_I_V": 960,
+      "population": 33388000,
+      "literacy": 93.9
     }
   },
   "LD": {
@@ -274,14 +274,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 940,
       "sex_ratio": 946,
       "name": "Lakshadweep",
-      "area": 32,
-      "density": 2149,
-      "population": 64000,
+      "density": 2000,
       "num_schools": 41,
-      "literacy": 92.3
+      "area": 32,
+      "enrollment_sex_ratio_I_V": 940,
+      "literacy": 92.3,
+      "population": 64000
     }
   },
   "MH": {
@@ -289,14 +289,14 @@ var stateData = {
       "std_v_division": 20.3
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 880,
       "sex_ratio": 925,
       "name": "Maharashtra",
-      "area": 307713,
       "density": 365,
-      "population": 112373000,
       "num_schools": 98213,
-      "literacy": 82.9
+      "area": 307713,
+      "enrollment_sex_ratio_I_V": 880,
+      "literacy": 82.9,
+      "population": 112373000
     }
   },
   "ML": {
@@ -304,14 +304,14 @@ var stateData = {
       "std_v_division": 10.7
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 1000,
       "sex_ratio": 986,
       "name": "Meghalaya",
-      "area": 22429,
       "density": 132,
-      "literacy": 75.5,
       "num_schools": 13277,
-      "population": 2964000
+      "area": 22429,
+      "enrollment_sex_ratio_I_V": 1000,
+      "population": 2964000,
+      "literacy": 75.5
     }
   },
   "MN": {
@@ -319,14 +319,14 @@ var stateData = {
       "std_v_division": 52.5
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 980,
       "sex_ratio": 987,
       "name": "Manipur",
-      "area": 22327,
-      "density": 115,
-      "literacy": 79.9,
+      "density": 121,
       "num_schools": 4865,
-      "population": 2722000
+      "area": 22327,
+      "enrollment_sex_ratio_I_V": 980,
+      "population": 2722000,
+      "literacy": 79.9
     }
   },
   "MP": {
@@ -334,14 +334,14 @@ var stateData = {
       "std_v_division": 19.4
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 900,
       "sex_ratio": 930,
       "name": "Madhya Pradesh",
-      "area": 308245,
-      "density": 236,
-      "literacy": 70.6,
+      "density": 235,
       "num_schools": 142587,
-      "population": 72598000
+      "area": 308245,
+      "enrollment_sex_ratio_I_V": 900,
+      "population": 72598000,
+      "literacy": 70.6
     }
   },
   "MZ": {
@@ -349,14 +349,14 @@ var stateData = {
       "std_v_division": 27.7
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 930,
       "sex_ratio": 975,
       "name": "Mizoram",
-      "area": 21081,
-      "density": 52,
-      "literacy": 91.6,
+      "density": 51,
       "num_schools": 3072,
-      "population": 1091000
+      "area": 21081,
+      "enrollment_sex_ratio_I_V": 930,
+      "population": 1091000,
+      "literacy": 91.6
     }
   },
   "NL": {
@@ -364,14 +364,14 @@ var stateData = {
       "std_v_division": 21.2
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 950,
       "sex_ratio": 931,
       "name": "Nagaland",
-      "area": 16579,
       "density": 119,
-      "population": 1981000,
       "num_schools": 2799,
-      "literacy": 80.1
+      "area": 16579,
+      "enrollment_sex_ratio_I_V": 950,
+      "literacy": 80.1,
+      "population": 1981000
     }
   },
   "OR": {
@@ -379,14 +379,14 @@ var stateData = {
       "std_v_division": 26.6
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 940,
       "sex_ratio": 978,
       "name": "Odisha",
-      "area": 155707,
-      "density": 270,
-      "literacy": 73.5,
+      "density": 269,
       "num_schools": 68978,
-      "population": 41947000
+      "area": 155707,
+      "enrollment_sex_ratio_I_V": 940,
+      "population": 41947000,
+      "literacy": 73.5
     }
   },
   "PB": {
@@ -394,14 +394,14 @@ var stateData = {
       "std_v_division": 47.9
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 830,
       "sex_ratio": 893,
       "name": "Punjab",
-      "area": 50362,
-      "density": 551,
-      "population": 27704000,
+      "density": 550,
       "num_schools": 28776,
-      "literacy": 76.7
+      "area": 50362,
+      "enrollment_sex_ratio_I_V": 830,
+      "literacy": 76.7,
+      "population": 27704000
     }
   },
   "PY": {
@@ -409,14 +409,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 950,
       "sex_ratio": 1038,
       "name": "Puducherry",
-      "area": 479,
-      "density": 2547,
-      "population": 1244000,
+      "density": 2597,
       "num_schools": 719,
-      "literacy": 86.6
+      "area": 479,
+      "enrollment_sex_ratio_I_V": 950,
+      "literacy": 86.6,
+      "population": 1244000
     }
   },
   "RJ": {
@@ -424,14 +424,14 @@ var stateData = {
       "std_v_division": 28.2
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 870,
       "sex_ratio": 926,
       "name": "Rajasthan",
-      "area": 342239,
       "density": 200,
-      "literacy": 67.1,
       "num_schools": 107931,
-      "population": 68621000
+      "area": 342239,
+      "enrollment_sex_ratio_I_V": 870,
+      "population": 68621000,
+      "literacy": 67.1
     }
   },
   "SK": {
@@ -439,14 +439,14 @@ var stateData = {
       "std_v_division": ""
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 890,
       "sex_ratio": 889,
       "name": "Sikkim",
-      "area": 7096,
-      "density": 86,
-      "literacy": 82.2,
+      "density": 85,
       "num_schools": 1279,
-      "population": 608000
+      "area": 7096,
+      "enrollment_sex_ratio_I_V": 890,
+      "population": 608000,
+      "literacy": 82.2
     }
   },
   "TL": {
@@ -454,14 +454,14 @@ var stateData = {
       "std_v_division": 30.4
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 930,
       "sex_ratio": 987,
       "name": "Telangana",
-      "area": 114840,
-      "density": 314,
-      "population": 35200000,
+      "density": 306,
       "num_schools": 40818,
-      "literacy": 66.5
+      "area": 114840,
+      "enrollment_sex_ratio_I_V": 930,
+      "literacy": 66.5,
+      "population": 35200000
     }
   },
   "TN": {
@@ -469,14 +469,14 @@ var stateData = {
       "std_v_division": 21.4
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 950,
       "sex_ratio": 995,
       "name": "Tamil Nadu",
-      "area": 130058,
-      "density": 555,
-      "population": 72139000,
+      "density": 554,
       "num_schools": 57539,
-      "literacy": 80.3
+      "area": 130058,
+      "enrollment_sex_ratio_I_V": 950,
+      "literacy": 80.3,
+      "population": 72139000
     }
   },
   "TR": {
@@ -484,14 +484,14 @@ var stateData = {
       "std_v_division": 19.9
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 961,
       "name": "Tripura",
-      "area": 10486,
       "density": 350,
-      "population": 3671000,
       "num_schools": 4844,
-      "literacy": 87.8
+      "area": 10486,
+      "enrollment_sex_ratio_I_V": 960,
+      "literacy": 87.8,
+      "population": 3671000
     }
   },
   "UP": {
@@ -499,50 +499,50 @@ var stateData = {
       "std_v_division": 22.6
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 950,
       "sex_ratio": 908,
       "name": "Uttar Pradesh",
-      "area": 240928,
-      "density": 829,
-      "literacy": 69.7,
+      "density": 828,
       "num_schools": 245919,
-      "population": 199581000
+      "area": 240928,
+      "enrollment_sex_ratio_I_V": 950,
+      "population": 199581000,
+      "literacy": 69.7
     }
   },
   "UT": {
     "ASER": {
-      "std_v_division": 37.0
+      "std_v_division": 37
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 890,
       "sex_ratio": 963,
       "name": "Uttarakhand",
-      "area": 53483,
       "density": 189,
-      "literacy": 79.6,
       "num_schools": 23660,
-      "population": 10117000
+      "area": 53483,
+      "enrollment_sex_ratio_I_V": 890,
+      "population": 10117000,
+      "literacy": 79.6
     }
   },
   "WB": {
     "ASER": {
-      "std_v_division": 29.0
+      "std_v_division": 29
     },
     "DISE": {
-      "enrollment_sex_ratio_I_V": 960,
       "sex_ratio": 947,
       "name": "West Bengal",
-      "area": 88752,
-      "density": 1028,
-      "population": 991348000,
+      "density": 11169,
       "num_schools": 95723,
-      "literacy": 77.1
+      "area": 88752,
+      "enrollment_sex_ratio_I_V": 960,
+      "literacy": 77.1,
+      "population": 991348000
     }
   }
 };
 
 var indiaData = {
 
-	states: stateData
+  states: stateData
 
 };
